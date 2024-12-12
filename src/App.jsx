62f142fda4719b8d3cf3e35bf9f5034fe1803eec
Antoine Stoykov <antoinestoykov1@gmail.com>
2024-12-12 09:22:06 +0100

    replace incorect code by a code that work and that create the local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,47 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 // import TodoList from './components/TodoList'
 // import TodoInput from './components/TodoInput'
 // import AddTodo from './components/AddTodo'
 
 function App() {
-  const [task, setTask] = useState('');
-  const [taskList, setTaskList] = useState([]);
+  const [task, setTask] = useState(() => {
+    let todo = localStorage.getItem("todo");
+    if(todo === null)
+      return "";
+    else 
+      return JSON.parse(todo);
+  })
+
+  useEffect(() => {
+    localStorage.setItem("task", JSON.stringify(task))
+  }, [task])
+
+  const [taskList, setTaskList] = useState(() => {
+    const savedTaskList = localStorage.getItem("taskList");
+    return savedTaskList ? JSON.parse(savedTaskList) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("taskList", JSON.stringify(taskList))
+  }, [taskList])
 
   // Fonction pour gérer l'ajout d'une tâche
   const addTask = (event) => {
     event.preventDefault();
     if (task.trim()) { // Prend la valeur de l'input sans espaces inutiles
-      setTaskList([...taskList, task]); // ajouter la valeur de l'input dans la liste taskList
+      setTaskList([...taskList, {text: task, completed: false}]); // ajouter la valeur de l'input dans la liste taskList
       setTask(''); // Réinitialise mon input
     }
   };
-    
+
+  // Fonction pour marquer une tâche comme terminée
+  const finishedTask = (index) => {
+    const updatedList = [...taskList]; // Créer une copie de la liste des tâches
+    updatedList[index].completed = !updatedList[index].completed; // Inverse l'atat de completed
+    setTaskList(updatedList); // Met à jour l'état avec la nouvelle liste modifiée
+  }
+
   return (
     <>
       <div className='app'>
@@ -29,26 +54,27 @@ function App() {
           type="text" 
           placeholder='Entrez votre tâche...'
           value={task}
-          onChange={(event) => setTask(event.target.value)}
+          onChange={(event) => setTask(event.target.value)} // Met à jour l'état de l'input
           />
 
           {/*Champ button pour créer une tâche*/}
           <button type='submit'>Add</button>
         </form>
+
         <main>
         <ul>
           {/*Liste avec les différentes tâches*/}
           {taskList.map((task, index) => ( 
-            <li key={index}>
+            <li key={index}
               style={{
-                textDecoration: taskItem.completed ? 'line-trough': 'none', // Barre le texte si la tâche est terminée
+                textDecoration: task.completed ? 'line-through': 'none', // Barre le texte si la tâche est terminée
               }}
+              >
               <input type="checkbox" 
-              checked={taskItem.completed}
-              onChange={() => addTask(index)}
-              onClick={finishedTask}
+              checked={task.completed}
+              onChange={() => finishedTask(index)}
               />
-              {task}
+              {task.text}
               </li>
           ))}
         </ul>
