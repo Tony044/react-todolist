2586575e53e149d6b2190d22598575ac08de8f33
Antoine Stoykov <antoinestoykov1@gmail.com>
2024-12-13 17:54:38 +0100

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-// import TodoList from './components/TodoList'
-// import TodoInput from './components/TodoInput'
-// import AddTodo from './components/AddTodo'
 
 function App() {
   const [task, setTask] = useState(() => {
-    let todo = localStorage.getItem("todo");
-    if(todo === null)
-      return "";
-    else 
-      return JSON.parse(todo);
+    let todo = localStorage.getItem("task");
+    // if(todo === null)
+    //   return "";
+    // else 
+      return todo ? JSON.parse(todo) : [];
   })
 
-  useEffect(() => {
-    localStorage.setItem("task", JSON.stringify(task))
-  }, [task])
+  // useEffect(() => {
+  //   localStorage.setItem("task", JSON.stringify(task))
+  // }, [task])
 
   const [taskList, setTaskList] = useState(() => {
     const savedTaskList = localStorage.getItem("taskList");
@@ -30,7 +27,7 @@ function App() {
   const addTask = (event) => {
     event.preventDefault();
     if (task.trim()) { // Prend la valeur de l'input sans espaces inutiles
-      setTaskList([...taskList, {text: task, completed: false}]); // ajouter la valeur de l'input dans la liste taskList
+      setTaskList([...taskList, {id: Date.now(), text: task, completed: false}]); // ajouter la valeur de l'input dans la liste taskList
       setTask(''); // Réinitialise mon input
     }
   };
@@ -41,6 +38,23 @@ function App() {
     updatedList[index].completed = !updatedList[index].completed; // Inverse l'atat de completed
     setTaskList(updatedList); // Met à jour l'état avec la nouvelle liste modifiée
   }
+
+  // Fonction pour supprimer une tâche
+  const deleteTask = (taskId) => {
+    // Filtrer la tâche à supprimer dans le localStorage
+    const updatedTasks = taskList.filter(task => task.id !== taskId);
+
+    // Mettre à jour l'état avec la nouvelle liste sans la tâche supprimée
+    setTaskList(updatedTasks);
+
+    // Sauvegarder la liste mise à jour dans le localStorage
+    localStorage.setItem('taskList', JSON.stringify(updatedTasks));
+  };
+
+  // Sauvegarder les tâches dans le localStorage chaque fois que todo change
+  useEffect(() => {
+    localStorage.setItem('task', JSON.stringify(task));
+  }, [task]); // cette fonction est appelé à chaque fois que task change
 
   return (
     <>
@@ -70,11 +84,19 @@ function App() {
                 textDecoration: task.completed ? 'line-through': 'none', // Barre le texte si la tâche est terminée
               }}
               >
+
               <input type="checkbox" 
               checked={task.completed}
               onChange={() => finishedTask(index)}
               />
               {task.text}
+
+              <button 
+                onClick={() => deleteTask(task.id)} className='deleteButton'
+                style={{
+                  textDecoration: task.completed ?! 'line-through': 'true', // Barre le texte si la tâche est terminée
+                }}
+                >X</button>
               </li>
           ))}
         </ul>
